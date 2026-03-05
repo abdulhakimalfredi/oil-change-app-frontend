ae1863e429d830a8a0df811bd65c7fa57f0a87b2
Abdulhakim <abdulhakimalfredi@gmail.com>
2026-03-05 16:52:14 +0300

    Fix API baseURL protocol and path

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'oil-change-app-backend-production.up.railway.app/api',
+    baseURL: 'https://oil-change-app-backend-production.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
